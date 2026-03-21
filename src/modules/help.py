5756d3469fb89ab9aec7656e8920c5c1cf93b224
gusdev <sounoob750@gmail.com>
2026-03-20 23:54:21 -0300

    resolved tiny bug in help module

--- src/modules/help.py
+++ src/modules/help.py
@@ -34,7 +34,7 @@ def get_header() -> dict:
     """
     return header
 
-def run(parameters : list, modules_home: str) -> int:
+def run(parameters : list, modules_home: str, *args) -> int:
     """
     Run the module
 
