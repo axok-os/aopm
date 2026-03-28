f2b9722af78adca3df05f1076b2556a9ecffe8f4
gusdev <sounoob750@gmail.com>
2026-03-27 22:29:16 -0300

    Refactor errors in core.py (better errors)

--- src/core.py
+++ src/core.py
@@ -19,7 +19,7 @@ import os
 
 # try import the AOPM API
 try:
-    import aopmAPI
+    import aopmAPI as aopm
 except ImportError:
     print("Cant import AOPM Module :(")
     sys.exit(1)
@@ -31,7 +31,7 @@ except ImportError:
 
 # where the config folder are
 # NOTE: this probably will be changed with the "configure" command
-config_path = "/home/gustavo/Projetos/axok-os/aopm/configs"
+config_path = "configs"
 
 # the config file
 config_file = f"{config_path}/aopm.conf"
@@ -59,9 +59,9 @@ for section in essential_sections:
             if conf.has_option(section, option):
                 pass
             else:
-                aopmAPI.error(f"The config file dont have the key: '{option}' in '{section}' :(", True)
+                aopm.error(f"The config file dont have the key: '{option}' in '{section}' :(", True)
     else:
-        aopmAPI.error(f"The config file dont have the section '{section}' :(", True)
+        aopm.error(f"The config file dont have the section '{section}' :(", True)
 
 # get the variables from config file
 modules_path = conf.get("Paths", "modules_path")
@@ -73,12 +73,12 @@ packages_path = conf.get("Paths", "packages_path")
 
 # check if the user is root
 if os.geteuid() != 0:
-    aopmAPI.error("To run AOPM you must be root :(", True)
+    aopm.error("To run AOPM you must be root!", True)
 
 # check the arguments
 argc = len(sys.argv)
 if argc < 2:
-    aopmAPI.error("Too few arguments :(\nTry: aopm help", True)
+    aopm.error("Too few arguments :(\nTry: aopm help me", True)
 
 # define the module and parameters
 
@@ -96,14 +96,14 @@ module = importlib.util.module_from_spec(spec)
 try:
     spec.loader.exec_module(module)
 except FileNotFoundError:
-    aopmAPI.error(f"No Module named: '{module_to_search}' found :(", True)
+    aopm.error(f"No Module named: '{module_to_search}' found :(", True)
 
 if hasattr(module, "get_header"):
     module_header = module.get_header()
 else:
-    aopmAPI.error(f"The module: '{module_to_search}' dont have the get_header function :(", True)
+    aopm.error(f"The module: '{module_to_search}' dont have the 'get_header' function :(", True)
 
-module_reader = aopmAPI.Api_header(module_header)
+module_reader = aopm.Api_header(module_header)
 if module_reader.check():
     if hasattr(module, "run"):
         exit_code = module.run(parameters_to_module, # parameters
@@ -115,7 +115,6 @@ if module_reader.check():
                             )
         sys.exit(exit_code)
     else:
-        aopmAPI.error(f"The module: '{module_to_search}' dont have the run function :(", True)
+        aopm.error(f"The module: '{module_to_search}' dont have the 'run' function :(", True)
 else:
-    print("Cant verify the API Header :(")
-    sys.exit(1)
+    aopm.error(f"The module: '{module_to_search}' dont have a valid header :(", True)
